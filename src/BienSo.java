8d6782a6bddc66f7029e5f02bd53541f1744e7de
HungAnh3948-livefa <nguyenhunganh3948@gmail.com>
2026-02-09 15:37:08 +0700

    Da them 1 file la: BienSo va thay doi o file HelloWorld co lien quan den scanner

---
+++ src/BienSo.java
@@ -0,0 +1,6 @@
+public class BienSo {
+    public static void main(String[] args) {
+        int a = 5 + 9*3 - 10/2;
+        System.out.println(a);
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !");
         System.out.println("Tôi tên là fwfewf");
+
+        int id = 101;
+        String item = "Sach";
+        double cost = 145.500;
+        System.out.printf("ID: %d - Ten: %s - Gia: %.3f VNĐ\n", id, item, cost);
+        // Kết quả: ID: 101 - Ten: Sach - Gia: 50.00 VNĐ
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in); //Tạo đối tượng scanner.
+        System.out.println("Tên của tôi là: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Tuổi: ");
+        int age = scanner.nextInt();
+
+        System.out.println("______________________");
+        System.out.print("Tên của bạn là: " + name + " | " + "Tuổi: " + age);
     }
 }
